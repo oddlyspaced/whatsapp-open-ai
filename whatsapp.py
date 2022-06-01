135c250630b8d4f9cb1d73bb6d574f0495fc5e86
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-06-01 13:08:29 +0530

    whatsapp: wrap members and functions in WhatsAppHandler class

--- whatsapp.py
+++ whatsapp.py
@@ -8,19 +8,23 @@ import enum
 class WhatsAppState(enum.Enum):
     AskingForLogin = 1
 
-# setting up chrome instance
-options = ChromeOptions()
-options.add_argument("--user-data-dir=" + config.data_direc)
+class WhatsAppHandler():
+    def __init__(self) -> None:
+        driver = None
+    
+    def load_driver(self) -> None:
+        # setting up chrome instance
+        options = ChromeOptions()
+        options.add_argument("--user-data-dir=" + config.data_direc)
+        self.driver = webdriver.Chrome(options=options)
 
-# launching driver
-driver = webdriver.Chrome(options=options)
+    def launch_whatsapp(self) -> None:
+        self.driver.get("https://web.whatsapp.com")
 
-driver.get("https://web.whatsapp.com")
-
-# function to wait for whatsapp to load
-def check_whatsapp_state():
-    try :
-        driver.find_element_by_class_name("_2WuPw")
-        return WhatsAppState.AskingForLogin
-    except:
-        pass
+    # function to wait for whatsapp to load
+    def check_whatsapp_state(self) -> WhatsAppState:
+        try :
+            self.driver.find_element_by_class_name("_2WuPw")
+            return WhatsAppState.AskingForLogin
+        except:
+            pass
