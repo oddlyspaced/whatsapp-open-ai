bdbb8f390bfff65ae51ec35e2600a097764a591c
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-06-01 12:51:59 +0530

    whatsapp: import sample config and set data directory path for chrome

--- whatsapp.py
+++ whatsapp.py
@@ -1,3 +1,11 @@
+from distutils.command.config import config
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from config import ProjectConfig as config
 
-driver = webdriver.Chrome()
+# setting up chrome instance
+options = ChromeOptions()
+options.add_argument("--user-data-dir=" + config.data_direc)
+
+# launching driver
+driver = webdriver.Chrome(options=options)
